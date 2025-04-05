ebb8b76eb94e290155eaef7ebe69c8a98ae0efaa
Yujun Qin <yamstudio@users.noreply.github.com>
2025-04-05 00:27:10 -0400

    Add budget model

--- src/WebApi/Migrations/20250405040017_CreateBudgetTable.Designer.cs
+++ src/WebApi/Migrations/20250405040017_CreateBudgetTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YamStudio.Budget.WebApi.Data;
 
@@ -11,9 +12,11 @@ using YamStudio.Budget.WebApi.Data;
 namespace YamStudio.Budget.WebApi.Migrations
 {
     [DbContext(typeof(BudgetDbContext))]
-    partial class BudgetDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250405040017_CreateBudgetTable")]
+    partial class CreateBudgetTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
